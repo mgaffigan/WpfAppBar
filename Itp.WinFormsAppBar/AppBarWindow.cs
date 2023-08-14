e32e466235c25a4f03238a00c67df5e77e50175c
mgaffigan <mgaffigan@DESKTOP-7NA0EGF>
2023-08-14 17:22:43 -0500

    Cloned from WPF

---
+++ Itp.WinFormsAppBar/AppBarWindow.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Itp.WpfAppBar
+{
+    using System.Windows.Media;
+    using static NativeMethods;
+
+    public class AppBarWindow : Window
+    {
+        private bool IsAppBarRegistered;
+        private bool IsInAppBarResize;
+
+        static AppBarWindow()
+        {
+            ShowInTaskbarProperty.OverrideMetadata(typeof(AppBarWindow), new FrameworkPropertyMetadata(false));
+            MinHeightProperty.OverrideMetadata(typeof(AppBarWindow), new FrameworkPropertyMetadata(20d, MinMaxHeightWidth_Changed));
+            MinWidthProperty.OverrideMetadata(typeof(AppBarWindow), new FrameworkPropertyMetadata(20d, MinMaxHeightWidth_Changed));
+            MaxHeightProperty.OverrideMetadata(typeof(AppBarWindow), new FrameworkPropertyMetadata(MinMaxHeightWidth_Changed));
+            MaxWidthProperty.OverrideMetadata(typeof(AppBarWindow), new FrameworkPropertyMetadata(MinMaxHeightWidth_Changed));
+        }
+
+        public AppBarWindow()
+        {
+            this.WindowStyle = WindowStyle.None;
+            this.ResizeMode = ResizeMode.NoResize;
+            this.Topmost = true;
+        }
+
+        public AppBarDockMode DockMode
+        {
+            get { return (AppBarDockMode)GetValue(DockModeProperty); }
+            set { SetValue(DockModeProperty, value); }
+        }
+
+        public static readonly DependencyProperty DockModeProperty =
+            DependencyProperty.Register("DockMode", typeof(AppBarDockMode), typeof(AppBarWindow),
+                new FrameworkPropertyMetadata(AppBarDockMode.Left, DockLocation_Changed));
+
+        public MonitorInfo Monitor
+        {
+            get { return (MonitorInfo)GetValue(MonitorProperty); }
+            set { SetValue(MonitorProperty, value); }
+        }
+
+        public static readonly DependencyProperty MonitorProperty =
+            DependencyProperty.Register("Monitor", typeof(MonitorInfo), typeof(AppBarWindow),
+                new FrameworkPropertyMetadata(null, DockLocation_Changed));
+
+        public int DockedWidthOrHeight
+        {
+            get { return (int)GetValue(DockedWidthOrHeightProperty); }
+            set { SetValue(DockedWidthOrHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty DockedWidthOrHeightProperty =
+            DependencyProperty.Register("DockedWidthOrHeight", typeof(int), typeof(AppBarWindow),
+                new FrameworkPropertyMetadata(200, DockLocation_Changed, DockedWidthOrHeight_Coerce));
+
+        private static object DockedWidthOrHeight_Coerce(DependencyObject d, object baseValue)
+        {
+            var @this = (AppBarWindow)d;
+            var newValue = (int)baseValue;
+
+            switch (@this.DockMode)
+            {
+                case AppBarDockMode.Left:
+                case AppBarDockMode.Right:
+                    return BoundIntToDouble(newValue, @this.MinWidth, @this.MaxWidth);
+
+                case AppBarDockMode.Top:
+                case AppBarDockMode.Bottom:
+                    return BoundIntToDouble(newValue, @this.MinHeight, @this.MaxHeight);
+
+                default: throw new NotSupportedException();
+            }
+        }
+
+        private static int BoundIntToDouble(int value, double min, double max)
+        {
+            if (min > value)
+            {
+                return (int)Math.Ceiling(min);
+            }
+            if (max < value)
+            {
+                return (int)Math.Floor(max);
+            }
+
+            return value;
+        }
+
+        private static void MinMaxHeightWidth_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            d.CoerceValue(DockedWidthOrHeightProperty);
+        }
+
+        private static void DockLocation_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var @this = (AppBarWindow)d;
+
+            if (@this.IsAppBarRegistered)
+            {
+                @this.OnDockLocationChanged();
+            }
+        }
+
+        protected override void OnDpiChanged(DpiScale oldDpi, DpiScale newDpi)
+        {
+            base.OnDpiChanged(oldDpi, newDpi);
+
+            OnDockLocationChanged();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // add the hook, setup the appbar
+            var source = (HwndSource)PresentationSource.FromVisual(this);
+
+            if (!ShowInTaskbar)
+            {
+                var exstyle = (ulong)GetWindowLongPtr(source.Handle, GWL_EXSTYLE);
+                exstyle |= (ulong)((uint)WS_EX_TOOLWINDOW);
+                SetWindowLongPtr(source.Handle, GWL_EXSTYLE, unchecked((IntPtr)exstyle));
+            }
+
+            source.AddHook(WndProc);
+
+            var abd = GetAppBarData();
+            SHAppBarMessage(ABM.NEW, ref abd);
+
+            // set our initial location
+            this.IsAppBarRegistered = true;
+            OnDockLocationChanged();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            if (e.Cancel)
+            {
+                return;
+            }
+
+            if (IsAppBarRegistered)
+            {
+                var abd = GetAppBarData();
+                SHAppBarMessage(ABM.REMOVE, ref abd);
+                IsAppBarRegistered = false;
+            }
+        }
+
+        private int WpfDimensionToDesktop(double dim)
+        {
+            var dpi = VisualTreeHelper.GetDpi(this);
+
+            return (int)Math.Ceiling(dim * dpi.PixelsPerDip);
+        }
+
+        private double DesktopDimensionToWpf(double dim)
+        {
+            var dpi = VisualTreeHelper.GetDpi(this);
+
+            return dim / dpi.PixelsPerDip;
+        }
+
+        private void OnDockLocationChanged()
+        {
+            if (IsInAppBarResize)
+            {
+                return;
+            }
+
+            var abd = GetAppBarData();
+            abd.rc = (RECT)GetSelectedMonitor().ViewportBounds;
+
+            SHAppBarMessage(ABM.QUERYPOS, ref abd);
+
+            var dockedWidthOrHeightInDesktopPixels = WpfDimensionToDesktop(DockedWidthOrHeight);
+            switch (DockMode)
+            {
+                case AppBarDockMode.Top:
+                    abd.rc.bottom = abd.rc.top + dockedWidthOrHeightInDesktopPixels;
+                    break;
+                case AppBarDockMode.Bottom:
+                    abd.rc.top = abd.rc.bottom - dockedWidthOrHeightInDesktopPixels;
+                    break;
+                case AppBarDockMode.Left:
+                    abd.rc.right = abd.rc.left + dockedWidthOrHeightInDesktopPixels;
+                    break;
+                case AppBarDockMode.Right:
+                    abd.rc.left = abd.rc.right - dockedWidthOrHeightInDesktopPixels;
+                    break;
+                default: throw new NotSupportedException();
+            }
+
+            SHAppBarMessage(ABM.SETPOS, ref abd);
+            IsInAppBarResize = true;
+            try
+            {
+                WindowBounds = (Rect)abd.rc;
+            }
+            finally
+            {
+                IsInAppBarResize = false;
+            }
+        }
+
+        private MonitorInfo GetSelectedMonitor()
+        {
+            var monitor = this.Monitor;
+            var allMonitors = MonitorInfo.GetAllMonitors();
+            if (monitor == null || !allMonitors.Contains(monitor))
+            {
+                monitor = allMonitors.First(f => f.IsPrimary);
+            }
+
+            return monitor;
+        }
+
+        private APPBARDATA GetAppBarData()
+        {
+            return new APPBARDATA()
+            {
+                cbSize = Marshal.SizeOf(typeof(APPBARDATA)),
+                hWnd = new WindowInteropHelper(this).Handle,
+                uCallbackMessage = AppBarMessageId,
+                uEdge = (int)DockMode
+            };
+        }
+
+        private static int _AppBarMessageId;
+        public static int AppBarMessageId
+        {
+            get
+            {
+                if (_AppBarMessageId == 0)
+                {
+                    _AppBarMessageId = RegisterWindowMessage("AppBarMessage_EEDFB5206FC3");
+                }
+
+                return _AppBarMessageId;
+            }
+        }
+
+        public IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_WINDOWPOSCHANGING && !IsInAppBarResize)
+            {
+                var wp = Marshal.PtrToStructure<WINDOWPOS>(lParam);
+                wp.flags |= SWP_NOMOVE | SWP_NOSIZE;
+                Marshal.StructureToPtr(wp, lParam, false);
+            }
+            else if (msg == WM_ACTIVATE)
+            {
+                var abd = GetAppBarData();
+                SHAppBarMessage(ABM.ACTIVATE, ref abd);
+            }
+            else if (msg == WM_WINDOWPOSCHANGED)
+            {
+                var abd = GetAppBarData();
+                SHAppBarMessage(ABM.WINDOWPOSCHANGED, ref abd);
+            }
+            else if (msg == AppBarMessageId)
+            {
+                switch ((ABN)(int)wParam)
+                {
+                    case ABN.POSCHANGED:
+                        OnDockLocationChanged();
+                        handled = true;
+                        break;
+                }
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private Rect WindowBounds
+        {
+            set
+            {
+                this.Left = DesktopDimensionToWpf(value.Left);
+                this.Top = DesktopDimensionToWpf(value.Top);
+                this.Width = DesktopDimensionToWpf(value.Width);
+                this.Height = DesktopDimensionToWpf(value.Height);
+            }
+        }
+    }
+
+    public enum AppBarDockMode
+    {
+        Left = 0,
+        Top,
+        Right,
+        Bottom
+    }
+}
---
+++ Itp.WinFormsAppBar/NativeMethods.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Itp.WpfAppBar
+{
+    internal static class NativeMethods
+    {
+        private const string User32 = "user32.dll";
+        private const string ShCore = "ShCore.dll";
+
+        public enum ABEdge
+        {
+            Left = 0,
+            Top,
+            Right,
+            Bottom,
+            None
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct APPBARDATA
+        {
+            public int cbSize;
+            public IntPtr hWnd;
+            public int uCallbackMessage;
+            public int uEdge;
+            public RECT rc;
+            public IntPtr lParam;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static explicit operator POINT(Point p)
+            {
+                return new POINT { X = (int)p.X, Y = (int)p.Y };
+            }
+
+            public static explicit operator Point(POINT r)
+            {
+                return new Point(r.X, r.Y);
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+
+            public int Width
+            {
+                get { return right - left; }
+            }
+
+            public int Height
+            {
+                get { return bottom - top; }
+            }
+
+            public void Offset(int dx, int dy)
+            {
+                left += dx;
+                top += dy;
+                right += dx;
+                bottom += dy;
+            }
+
+            public bool IsEmpty
+            {
+                get
+                {
+                    return left >= right || top >= bottom;
+                }
+            }
+
+            public static explicit operator Int32Rect(RECT r)
+            {
+                return new Int32Rect(r.left, r.top, r.Width, r.Height);
+            }
+
+            public static explicit operator Rect(RECT r)
+            {
+                return new Rect(r.left, r.top, r.Width, r.Height);
+            }
+
+            public static explicit operator RECT(Rect r)
+            {
+                return new RECT((int)r.Left, (int)r.Top, (int)r.Right, (int)r.Bottom);
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WINDOWPOS
+        {
+            public IntPtr hwnd;
+            public IntPtr hwndInsertAfter;
+            public int x;
+            public int y;
+            public int cx;
+            public int cy;
+            public int flags;
+
+            public Rect Bounds
+            {
+                get { return new Rect(x, y, cx, cy); }
+                set
+                {
+                    x = (int)value.X;
+                    y = (int)value.Y;
+                    cx = (int)value.Width;
+                    cy = (int)value.Height;
+                }
+            }
+        }
+
+        public const int
+            SWP_NOMOVE = 0x0002,
+            SWP_NOSIZE = 0x0001;
+
+        public const int
+            WM_ACTIVATE = 0x0006,
+            WM_WINDOWPOSCHANGED = 0x0047,
+            WM_SYSCOMMAND = 0x0112,
+            WM_WINDOWPOSCHANGING = 0x0046;
+
+        public const int
+            SC_MOVE = 0xF010;
+
+        public static int SC_FROM_WPARAM(IntPtr wparam)
+        {
+            // In WM_SYSCOMMAND messages, the four low-order bits of the wParam parameter are used internally by
+            // the system. To obtain the correct result when testing the value of wParam, an application must
+            // combine the value 0xFFF0 with the wParam value by using the bitwise AND operator.
+            return ((int)wparam & 0xfff0);
+        }
+
+        public enum ABM
+        {
+            NEW = 0,
+            REMOVE,
+            QUERYPOS,
+            SETPOS,
+            GETSTATE,
+            GETTASKBARPOS,
+            ACTIVATE,
+            GETAUTOHIDEBAR,
+            SETAUTOHIDEBAR,
+            WINDOWPOSCHANGED,
+            SETSTATE
+        }
+
+        public enum ABN
+        {
+            STATECHANGE = 0,
+            POSCHANGED,
+            FULLSCREENAPP,
+            WINDOWARRANGE
+        }
+
+        public const int
+            GWL_EXSTYLE = -20;
+
+        public const int
+            WS_EX_TOOLWINDOW = 0x00000080;
+
+        [DllImport("shell32.dll", ExactSpelling = true)]
+        public static extern uint SHAppBarMessage(ABM dwMessage, ref APPBARDATA pData);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int RegisterWindowMessage(string msg);
+
+        [DllImport("user32.dll", ExactSpelling = true, SetLastError = true)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint flags);
+
+        public static IntPtr GetWindowLongPtr(IntPtr hWnd, int index)
+            => IntPtr.Size == 4 ? GetWindowLongPtr32(hWnd, index) : GetWindowLongPtr64(hWnd, index);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        private static extern IntPtr GetWindowLongPtr32(IntPtr hWnd, int index);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
+        private static extern IntPtr GetWindowLongPtr64(IntPtr hWnd, int index);
+
+        public static IntPtr SetWindowLongPtr(IntPtr hWnd, int index, IntPtr newLong)
+            => IntPtr.Size == 4 ? SetWindowLongPtr32(hWnd, index, newLong) : SetWindowLongPtr64(hWnd, index, newLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        private static extern IntPtr SetWindowLongPtr32(IntPtr hWnd, int index, IntPtr newLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int index, IntPtr newLong);
+
+
+        [Flags]
+        public enum MONITORINFOF
+        {
+            PRIMARY = 0x1
+        }
+
+        [Flags]
+        public enum MONITOR_DPI_TYPE
+        {
+            MDT_EFFECTIVE_DPI = 0,
+            MDT_ANGULAR_DPI = 1,
+            MDT_RAW_DPI = 2,
+            MDT_DEFAULT = MDT_EFFECTIVE_DPI
+        }
+
+        private const int CCHDEVICENAME = 32;
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct MONITORINFOEX
+        {
+            public int cbSize;
+            public RECT rcMonitor;
+            public RECT rcWork;
+            public MONITORINFOF dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+            public string szDevice;
+        }
+
+        public delegate bool MonitorEnumDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
+
+        [DllImport(User32, CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFOEX lpmi);
+
+        [DllImport(User32)]
+        public static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumDelegate lpfnEnum, IntPtr dwData);
+
+        [DllImport(ShCore, ExactSpelling = true, PreserveSig = false)]
+        public static extern void GetDpiForMonitor(IntPtr hMonitor, MONITOR_DPI_TYPE dpiType, out int dpiX, out int dpiY);
+
+        //[DllImport(User32, ExactSpelling = true, SetLastError = true)]
+        //public static extern bool PhysicalToLogicalPointForPerMonitorDPI(IntPtr hWnd, POINT point);
+    }
+}
